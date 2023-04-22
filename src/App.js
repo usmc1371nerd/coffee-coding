b4556b4cfd3a6bc2d74122f1c59b5d610ead26da
usmc1371nerd <james.p.dumas@gmail.com>
2023-04-22 01:53:20 -0500

    intial website push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>Coffee and Coding</h1>
+        <h4>Join our  <a href="https://join.slack.com/t/coffee-and-coding-hq/shared_invite/zt-1u12s6yio-Tg6AKx9pXoD3WaTJjC6xSA"> <button className="slack">Slack</button></a></h4>
+       
+        <h6>First project idea build a proper full website... :) </h6>
+        
+        
       </header>
+      <section className="about">   
+      <div className="greetings">
+      <h1>Greetings to everyone if your reading this that means we are
+        still in the beginning stages of this project/ journey.. This is not built by
+        1 person but instead whoever joins and as a team we build and learn together. 
+        Hopefully people with a lot of experience joins or extends some helping knowledge
+        but this is and will always be for everyone. From someone just interested in code all the way to a jedi
+        developer. This is supposed to be a project to help build a community here in Birmingham
+        for all developers or people interested in coding, programming, or web development(whatever you want to call it). 
+      </h1>
+
+      </div>
+      </section>
+
+      <section className="links">
+    <h1>New to coding or programming.. Want to get started and can't afford
+      those expensive pesky college courses or bootcamps...</h1>
+    
+    <h5>   
+      <li>
+      <a href="https://www.theodinproject.com/"target="blank">Odin-Project</a>
+      </li>
+      <li>
+        <a href="https://freecodecamp.com"target="blank">freecodecamp</a>
+      </li>
+    </h5>
+    <h6>Don't forget the greatest university of all <a href="https://www.youtube.com/" target="blank">Youtube</a></h6>
+     </section>
+    
     </div>
   );
 }
